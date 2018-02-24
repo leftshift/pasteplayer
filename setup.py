b251a1fd37c87c7028b6573dff0a05571a13ec48
Adrian Schneider <ardy@ardy.io>
2018-02-24 18:37:28 +0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+with open("README.md", "r") as f:
+    long_descr = f.read()
+
+setup(name='pasteplayer',
+      version='1.0',
+      description='A thin mpv wrapper for on-the-fly playlist editing',
+      long_description=long_descr,
+      author='uberardy',
+      author_email='github@ardy.io',
+      license='GPLv3',
+      url='https://github.com/leftshift/pasteplayer',
+      py_modules=['pasteplayer'],
+      install_requires=['python-mpv'],
+      entry_points={
+          'console_scripts': ['pasteplayer = pasteplayer:main']
+      }
+)
